dd88bb16b22f9a2d84e6a5ef982545d300da76ca
AbdallahMohamed <devabdo2000>
2021-05-21 17:31:38 +0200

    Finish Sign up / waiting for confirmation page #105

---
+++ lib/UI/screens/SignupWaitingForConfirmation/SignupWaitingForConfirmation.dart
@@ -0,0 +1,100 @@
+import 'package:compound_app/UI/AppImages/appImages.dart';
+import 'package:compound_app/UI/appColors/appColors.dart';
+import 'package:compound_app/UI/sharedWidgets/logo.dart';
+import 'package:flutter/material.dart';
+
+class SignupWaitingForConfirmationScreen extends StatelessWidget {
+  static const routeName = '/SignupWaitingForConfirmationScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            LogoWidget(),
+            SizedBox(
+              height: 100,
+            ),
+            Image.asset(AppImages.appCheck),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Account Activation',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: AppColor.primaryColor,
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: 20,
+              ),
+              child: Text(
+                'We are working on activating your',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: AppColor.hintsColor,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'account and making sure of all your information.',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: AppColor.hintsColor,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'This process makes the community safe.',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: AppColor.hintsColor,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'and maintains privacy.',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: AppColor.hintsColor,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              onTap: () {
+                // Navigator.of(context).pushNamed(ReportIssueScreen.routeName);
+              },
+              child: Text(
+                'Call us',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.primaryColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
